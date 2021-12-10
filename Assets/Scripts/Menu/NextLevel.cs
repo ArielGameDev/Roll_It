ec85ffeba232c1388c764f4cba00fba8f26ae3b8
barakamram <barak55amram@gmail.com>
2021-12-10 16:37:50 +0200

    Menu

---
+++ Assets/Scripts/Menu/NextLevel.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour
+{
+    private int nextScene;
+    private int currScene;
+
+    private void Start()
+    {
+        currScene = SceneManager.GetActiveScene().buildIndex;
+        PlayerPrefs.SetInt("CurrScene", currScene);
+        PlayerPrefs.SetInt("NextScene", currScene);
+        nextScene = currScene + 1;
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            PlayerPrefs.SetInt("NextScene", nextScene);
+            SceneManager.LoadScene(3);
+        }
+    }
+    
+  
+}       
---
+++ Assets/Scripts/Menu/OnClick.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class OnClick : MonoBehaviour
+{
+	void Start()
+	{
+		Button button = this.GetComponent<Button>();
+		button.onClick.AddListener(LoadOnClick);
+		string name = this.GetComponent<Button>().gameObject.name;
+	}
+	void LoadOnClick()
+	{
+		SceneManager.LoadScene(name);
+	}
+}
