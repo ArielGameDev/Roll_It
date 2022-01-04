1059e35109112eb6f202e850424c6f1e566a9666
mikel <mikelk96@gmail.com>
2022-01-04 23:11:00 +0200

    change leves rewrite

--- Assets/Scripts/Menu/NextLevel.cs
+++ Assets/Scripts/Menu/NextLevel.cs
@@ -5,24 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class NextLevel : MonoBehaviour
 {
-    private int nextScene;
+    private int nextScene_asint;
     private int currScene;
     private string stage;
+
+    [SerializeField]
+    public Object nextScene;
 
     private void Start()
     {
         currScene = SceneManager.GetActiveScene().buildIndex;
         PlayerPrefs.SetInt("CurrScene", currScene);
         PlayerPrefs.SetInt("NextScene", currScene);
-        nextScene = currScene + 1;
+        nextScene_asint = currScene + 1;
     }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.tag == "Player")
         {
-            PlayerPrefs.SetInt("available", 1);
-            PlayerPrefs.SetInt("NextScene", nextScene);
-            SceneManager.LoadScene(3);
+            // PlayerPrefs.SetInt("available", 1);
+            // PlayerPrefs.SetInt("NextScene", nextScene_asint);
+            // SceneManager.LoadScene(3);
+
+            SceneManager.LoadScene(nextScene.name);
         }
     }
     
