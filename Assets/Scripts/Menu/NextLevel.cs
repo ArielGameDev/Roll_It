65708975da51277329dde8fd48a9ba257e49bfcd
nizan153 <nizan153@gmail.com>
2022-01-05 01:00:36 +0200

    update level orders and fix winner screen

--- Assets/Scripts/Menu/NextLevel.cs
+++ Assets/Scripts/Menu/NextLevel.cs
@@ -21,13 +21,22 @@ public class NextLevel : MonoBehaviour
     }
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.collider.tag == "Player")
+        if (collision.collider.tag == "Player" && currScene != 10)
         {
             PlayerPrefs.SetInt("available", 1);
             PlayerPrefs.SetInt("NextScene", nextScene_asint);
             // SceneManager.LoadScene(3);
 
             SceneManager.LoadScene(nextScene.name);
+        }
+        else if(collision.collider.tag == "Player" && currScene == 10)
+        {
+            nextScene = 4;
+            currScene = 4;
+            PlayerPrefs.SetInt("CurrScene", currScene);
+            PlayerPrefs.SetInt("NextScene", currScene);
+            SceneManager.LoadScene(3);
+            
         }
     }
     
